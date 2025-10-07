9325e16434d0d60339769aecedc9cf4b131691b9
Kunakbaev <tyrep2017@mail.ru>
2025-10-07 18:09:53 +0300

    Updated show_help_msg target and fixed display_triangles script

--- tests/display/display_triangles_from_file.py
+++ tests/display/display_triangles_from_file.py
@@ -5,8 +5,6 @@ from display_triangles import create_3d_triangles
 import mpl_toolkits
 
 if __name__ == "__main__":
-  print(f"mpl_toolkit version: {mpl_toolkits.version}")
-
   if (len(sys.argv) != 2):
     print("This script takes only one argument: \n" \
     "path to the file with triangles coordinates, that are going to be processed...")
@@ -22,18 +20,29 @@ if __name__ == "__main__":
     assert(len(lines) > 0)
     num_triangles = int(lines[0])
     triangles = []
-    for i in range(1, num_triangles + 1):
+
+    i = 1
+    while i < len(lines):
       if len(lines[i]) == 0:
+        i += 1
         continue
+
       values = []
       for j in range(3):
-        tmp = list(map(float, lines[i + j].split()))
+        tmp = list(map(float, lines[i].split()))
+        i += 1
         values += tmp
-      if len(values) == 0:
-        continue
+      # if len(values) == 0:
+      #   i += 3
+      #   continue
       
       triangle = np.array(values).reshape(3, 3)
+      print("triangle : ", triangle)
       triangles.append(triangle)
+
+      num_triangles -= 1
+      if num_triangles == 0:
+        break
 
     fig, ax = create_3d_triangles(triangles)
     plt.show()
